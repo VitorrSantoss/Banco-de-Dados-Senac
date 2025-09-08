2b7a5e9ca3334174c865a79a65f5f74bcabfbdc1
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-09-08 16:44:19 -0300

    Criação de tabelas

--- DataBase/Commands_Create.sql
+++ DataBase/Commands_Create.sql
@@ -1,10 +1,10 @@
 
-/*CRIANDO E USANDO O BANCO DE DADOS*/
+
 CREATE DATABASE loja_de_roupas;
 USE loja_de_roupas;
 
 
-/*CRIANDO AS TABELAS*/
+
 CREATE TABLE Produto(
   cod_unico INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(45) NOT NULL,
@@ -14,5 +14,48 @@ CREATE TABLE Produto(
   cor VARCHAR(45) NOT NULL,
   marca VARCHAR(45) NOT NULL
 )
+
+-- Criação de tabelas
+-- -----------------------------------------------------
+-- Table `lojaroupas`.`estoque`
+-- -----------------------------------------------------
+CREATE TABLE `lojaroupas`.`estoque` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_produto` INT NOT NULL,
+  `cor` VARCHAR(50) NOT NULL,
+  `tamanho` VARCHAR(10) NOT NULL,
+  `quantidade` INT NOT NULL DEFAULT 0,
+  `ultima_atualizacao` TIMESTAMP NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `lojaroupas`.`Fornecedores`
+-- -----------------------------------------------------
+CREATE TABLE  `lojaroupas`.`Fornecedores` (
+  `idFornecedores` INT NOT NULL AUTO_INCREMENT,
+  `razao_social` VARCHAR(255) NOT NULL,
+  `nome_fantasia` VARCHAR(255) NULL,
+  `cnpj` VARCHAR(18) NOT NULL,
+  `telefone` VARCHAR(20) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `Fornecedorescol` VARCHAR(45) NULL,
+  `nome_contato` VARCHAR(100) NOT NULL,
+  `cep` VARCHAR(9) NOT NULL,
+  `logradouro` VARCHAR(255) NOT NULL,
+  `numero` VARCHAR(10) NOT NULL,
+  `complemento` VARCHAR(100) NOT NULL,
+  `bairro` VARCHAR(100) NOT NULL,
+  `cidade` VARCHAR(100) NOT NULL,
+  `estado` VARCHAR(2) NOT NULL,
+  
+  
+  PRIMARY KEY (`idFornecedores`),
+  UNIQUE INDEX `razao_social_UNIQUE` (`razao_social` ASC) VISIBLE,
+  UNIQUE INDEX `cnpj_UNIQUE` (`cnpj` ASC) VISIBLE,
+  UNIQUE INDEX `telefone_UNIQUE` (`telefone` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+  ENGINE = InnoDB;
 
 
